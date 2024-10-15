5579ab0c5faac60aeb3afceba19215186e3093b5
P JAYANTH <pjayanthreddy789@gmail.com>
2024-10-15 18:39:56 +0530

    protfolio.js

--- protfolio.js
+++ protfolio.js
@@ -2,16 +2,16 @@ document.addEventListener('DOMContentLoaded', () => {
     const toggleButton = document.getElementById('dark-mode-toggle');
     const body = document.body;
 
-    // Load the saved theme preference from localStorage
-    const savedTheme = localStorage.getItem('theme');
-    if (savedTheme === 'dark') {
-        body.classList.add('dark-mode');
+    if (typeof(Storage) !== 'undefined') {
+        const savedTheme = localStorage.getItem('theme');
+        if (savedTheme === 'dark') {
+            body.classList.add('dark-mode');
+        }
     }
 
     toggleButton.addEventListener('click', () => {
         body.classList.toggle('dark-mode');
         
-        // Save the theme preference in localStorage
         if (body.classList.contains('dark-mode')) {
             localStorage.setItem('theme', 'dark');
         } else {
@@ -19,3 +19,4 @@ document.addEventListener('DOMContentLoaded', () => {
         }
     });
 });
+
